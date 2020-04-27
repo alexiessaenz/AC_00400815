a94c3270ca04184f3fc4c3faa65b350b50ce49cf
00400815@uca.edu.sv <16137238>
2020-04-26 19:10:32 -0600

    section

--- tarea_2_00400815.asm
+++ tarea_2_00400815.asm
@@ -1,14 +1,15 @@
-org	100h	;004 00815
+	org	100h	;004 00815
+	
+section	.text 
+	mov	ax,		0
+	mov	bl,		5
 
-mov	ax,		0
-mov	bl,		5
+	add	ax,		0
+	add	ax,		0
+	add	ax,		8
+	add	ax,		1
+	add	ax,		5
+	div	bl
+	mov [208h], 		al	
 
-add	ax,		0
-add	ax,		0
-add	ax,		8
-add	ax,		1
-add	ax,		5
-div	bl
-mov [208h], 		al	
-
-int 20h
+	int 20h
